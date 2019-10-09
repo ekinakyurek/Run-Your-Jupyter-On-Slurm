bd0b8f0cfa3c3e1e0d8c1d46027c94fc45d9987d
ekinakyurek <akyurekekin@gmail.com>
2019-10-09 14:22:08 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Run-Your-Jupyter-On-Slurm
+
+This a single useful script to run a jupyter notebook on a compute node and get the link to connect from your local computer.
+
+Just type below command and get your notebook working!
+
+```shell
+ sh run.sh hostname
+```
+
+You can also modify job configurations in the same file. Note that exiting from script does not terminate the job on the SLURM.
+
+The script assumes that you have SSH Public Key Authentication to your login node, and you configure .ssh/known_hosts in your local computer.
+
+If you job cannot be taken immediately ( ~60s) in your SLURM the script will fail.
+
+Please feel free to make any improvement on the script via pull requests.
---
+++ jupy.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+: ${@?no positional parameters}
+REMOTE=$1
+SLURMFILE="./jupyter_submit.sh"
+[ -d $SLURMFILE ] && rm $SLURMFILE
+cat > $SLURMFILE <<EOF
+#!/bin/bash
+#SBATCH --job-name=JupiterNotebook
+#SBATCH --time=12:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --partition=gpu
+#SBATCH --constrain=xeon-e5
+#SBATCH --gres=gpu:volta:1
+#SBATCH --output=jupyter-%J.log
+
+# Source env variables
+source ~/.bashrc
+#echo
+#echo "============================== ENVIRONMENT VARIABLES ==============================="
+#env
+#echo "===================================================================================="
+#echo
+
+# Set stack size to unlimited
+echo "Setting stack size to unlimited..."
+ulimit -s unlimited
+ulimit -l unlimited
+ulimit -a
+echo
+# get tunneling info
+XDG_RUNTIME_DIR=""
+port=\$(shuf -i 6000-6999 -n1)
+node=\$(hostname -s)
+user=\$(whoami)
+
+# print tunneling instructions jupyter-log
+echo -e "ssh -N -L \${port}:\${node}:\${port} supercloud"
+
+jupyter notebook  --no-browser --port=\${port} --ip="*"
+EOF
+echo "sending job file to the ${REMOTE}:~/"
+scp $SLURMFILE ${REMOTE}:~/
+LOCAL_LOG_FOLDER=".jupyter_logs"
+[ ! -d $LOCAL_LOG_FOLDER ] && mkdir $LOCAL_LOG_FOLDER
+
+echo "just wait for 1 minute"
+ssh -T ${REMOTE} <<'ENDSSH'
+      sbatch  jupyter_submit.sh  && sleep 1m
+ENDSSH
+echo "getting log files from ${REMOTE}:~/"
+scp ${REMOTE}:~/jupyter*.log $LOCAL_LOG_FOLDER
+cd $LOCAL_LOG_FOLDER
+logfile=`ls -tp | grep -v /$ | head -1`
+tunnel=`grep 'ssh -N' $logfile`
+echo "creating the tunnel: $tunnel"
+jlink=`grep -i 'http' $logfile | tail -n -1 | grep -o 'http.*'`
+echo "You're ready! notebook is running on: $jlink"
+eval $tunnel
